6e10436d5f766472876c3ffc32f92bd5a0c5c983
Pablo Bello <pablobello@hey.com>
2024-05-05 00:12:35 +0100

    Add utils

---
+++ utils/utils.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var Validate = validator.New()
+
+func ParseJSON(r *http.Request, payload any) error {
+	if r.Body == nil {
+		return fmt.Errorf("missing request body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(payload)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	WriteJSON(w, status, map[string]string{"error": err.Error()})
+}
+
+func WriteInternalServerError(w http.ResponseWriter) {
+	message := "An unexpected error occurred. " +
+		"Please try again later or contact support if the problem persists."
+
+	WriteJSON(w,
+		http.StatusInternalServerError,
+		map[string]string{
+			"error":   "Internal Server Error",
+			"message": message,
+		})
+}
