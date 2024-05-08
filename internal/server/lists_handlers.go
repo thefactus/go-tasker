f897632fd48974cc052d2115aa6478bf609b8714
Pablo Bello <pablobello@hey.com>
2024-05-08 19:29:09 +0100

    Add GetListsHandler to retrieve lists

--- internal/server/lists_handlers.go
+++ internal/server/lists_handlers.go
@@ -6,6 +6,18 @@ import (
 	"todolist/utils"
 )
 
+func (s *Server) GetListsHandler(w http.ResponseWriter, r *http.Request) {
+	lists, err := s.db.GetLists()
+	if err != nil {
+		http.Error(w, "Error getting lists", http.StatusInternalServerError)
+		return
+	}
+
+	response := utils.PrepareJSONWithMessage("Lists retrieved successfully", lists)
+
+	utils.WriteJSON(w, http.StatusOK, response)
+}
+
 func (s *Server) PostListsHandler(w http.ResponseWriter, r *http.Request) {
 	var createListPayload types.CreateListPayload
 	if err := utils.ParseAndValidateJSON(w, r, &createListPayload); err != nil {
