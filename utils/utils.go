f66ce5071fc24477690ef8266a7110062b72a222
Pablo Bello <pablobello@hey.com>
2024-05-08 14:09:10 +0100

    Add new utils

--- utils/utils.go
+++ utils/utils.go
@@ -4,22 +4,50 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"reflect"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/iancoleman/strcase"
+	"gorm.io/gorm"
 )
 
 var Validate = validator.New()
 
-func ParseJSON(r *http.Request, payload any) error {
+func ParseAndValidateJSON(w http.ResponseWriter, r *http.Request, payload any) error {
 	if r.Body == nil {
-		return fmt.Errorf("missing request body")
+		WriteError(w, http.StatusBadRequest, fmt.Errorf("missing request body"))
+		return nil
 	}
 
-	return json.NewDecoder(r.Body).Decode(payload)
+	err := json.NewDecoder(r.Body).Decode(payload)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return err
+	}
+
+	// Validate the payload
+	if err := Validate.Struct(payload); err != nil {
+		var missingFields []string
+		for _, err := range err.(validator.ValidationErrors) {
+			field := err.Field()
+			tag := err.Tag()
+			if tag == "required" {
+				field = strings.ToLower(field)
+				missingFields = append(missingFields, field)
+			}
+		}
+		errorMessage :=
+			fmt.Sprintf("Missing required fields: %s", strings.Join(missingFields, ", "))
+		WriteError(w, http.StatusBadRequest, fmt.Errorf(errorMessage))
+		return err
+	}
+
+	return nil
 }
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
 }
@@ -28,7 +56,7 @@ func WriteError(w http.ResponseWriter, status int, err error) {
 	WriteJSON(w, status, map[string]string{"error": err.Error()})
 }
 
-func WriteInternalServerError(w http.ResponseWriter) {
+func WriteInternalServerError(w http.ResponseWriter, err error) {
 	message := "An unexpected error occurred. " +
 		"Please try again later or contact support if the problem persists."
 
@@ -38,4 +66,68 @@ func WriteInternalServerError(w http.ResponseWriter) {
 			"error":   "Internal Server Error",
 			"message": message,
 		})
+}
+
+func PrepareJSONWithMessage(message string, payload interface{}) map[string]interface{} {
+	response := map[string]interface{}{
+		"message": message,
+	}
+	payloadMap := createPayloadMap(payload)
+	response["data"] = payloadMap
+
+	jsonResponse := marshalAndUnmarshalJSON(response)
+	return jsonResponse
+}
+
+func createPayloadMap(payload interface{}) map[string]interface{} {
+	payloadMap := make(map[string]interface{})
+	val := reflect.ValueOf(payload)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	typeOfPayload := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		fieldName := typeOfPayload.Field(i).Name
+		snakeCaseName := toSnakeCase(fieldName)
+		fieldValue := val.Field(i).Interface()
+
+		if gormModel, ok := fieldValue.(gorm.Model); ok {
+			handleGormModelField(gormModel, payloadMap)
+		} else {
+			payloadMap[snakeCaseName] = fieldValue
+		}
+	}
+
+	return payloadMap
+}
+
+func handleGormModelField(gormModel gorm.Model, payloadMap map[string]interface{}) {
+	payloadMap["id"] = gormModel.ID
+	payloadMap["created_at"] = gormModel.CreatedAt
+	payloadMap["updated_at"] = gormModel.UpdatedAt
+	if gormModel.DeletedAt.Valid {
+		payloadMap["deleted_at"] = gormModel.DeletedAt.Time
+	}
+}
+
+func marshalAndUnmarshalJSON(response map[string]interface{}) map[string]interface{} {
+	jsonResp, err := json.Marshal(response)
+	if err != nil {
+		return nil
+	}
+
+	var jsonResponse map[string]interface{}
+	err = json.Unmarshal(jsonResp, &jsonResponse)
+	if err != nil {
+		return nil
+	}
+
+	return jsonResponse
+}
+
+func toSnakeCase(str string) string {
+	return strcase.ToSnake(str)
 }
