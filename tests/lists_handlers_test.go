2431f4a6190b0a5b66d904171da4f64123a9819d
Pablo Bello <pablobello@hey.com>
2024-05-08 19:25:44 +0100

    Add tests to get lists

--- tests/lists_handlers_test.go
+++ tests/lists_handlers_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"testing"
 	"todolist/internal/server"
 
@@ -20,6 +21,11 @@ var (
 	s  *http.Server
 	db *gorm.DB
 )
+
+type Response struct {
+	Data    []interface{} `json:"data"`
+	Message string        `json:"message"`
+}
 
 func TestMain(m *testing.M) {
 	s = server.NewServer()
@@ -62,39 +68,99 @@ func checkResponseCode(t *testing.T, expected, actual int) {
 	}
 }
 
-func TestCreateList(t *testing.T) {
-	clearTable()
+func TestLists(t *testing.T) {
+	t.Run("expects to get empty data", func(t *testing.T) {
+		clearTable()
 
-	payload := []byte(`{"title": "Tasks"}`)
-	req, _ := http.NewRequest("POST", "http://localhost:8080/api/v1/lists", bytes.NewReader(payload))
-	response := executeRequest(req)
+		req, _ := http.NewRequest("GET", "/api/v1/lists", nil)
+		response := executeRequest(req)
 
-	checkResponseCode(t, http.StatusOK, response.Code)
+		checkResponseCode(t, http.StatusOK, response.Code)
 
-	var result map[string]interface{}
-	err := json.Unmarshal(response.Body.Bytes(), &result)
-	if err != nil {
-		t.Errorf("Error unmarshalling response: %v", err)
-		return
-	}
+		var actual Response
+		json.Unmarshal(response.Body.Bytes(), &actual)
 
-	// Check for message
-	if message, ok := result["message"].(string); !ok || message != "List created successfully" {
-		t.Errorf("Expected message to be 'List created successfully'. Got %v", result["message"])
-	}
+		expected := Response{
+			Data:    []interface{}{},
+			Message: "Lists retrieved successfully",
+		}
 
-	// Check for id
-	if id, ok := result["data"].(map[string]interface{})["id"].(float64); !ok || id != 1 {
-		t.Errorf("Expected id to be 1. Got %v", result["data"].(map[string]interface{})["id"])
-	}
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("Expected '%v'. Got '%v'", expected, actual)
+		}
+	})
 
-	// Check for title
-	if title, ok := result["data"].(map[string]interface{})["title"].(string); !ok || title != "Tasks" {
-		t.Errorf("Expected title to be 'Tasks'. Got %v", result["data"].(map[string]interface{})["title"])
-	}
+	t.Run("expects to create a list", func(t *testing.T) {
+		clearTable()
 
-	// Check for CreatedAt presence
-	if _, ok := result["data"].(map[string]interface{})["created_at"]; !ok {
-		t.Errorf("Expected 'created_at' field to be present")
-	}
+		payload := []byte(`{"title": "Tasks"}`)
+		req, _ := http.NewRequest("POST", "/api/v1/lists", bytes.NewReader(payload))
+		response := executeRequest(req)
+
+		checkResponseCode(t, http.StatusCreated, response.Code)
+
+		var result map[string]interface{}
+		err := json.Unmarshal(response.Body.Bytes(), &result)
+		if err != nil {
+			t.Errorf("Error unmarshalling response: %v", err)
+			return
+		}
+
+		// Check for message
+		if message, ok := result["message"].(string); !ok || message != "List created successfully" {
+			t.Errorf("Expected message to be 'List created successfully'. Got %v", result["message"])
+		}
+
+		// Check for id
+		if id, ok := result["data"].(map[string]interface{})["id"].(float64); !ok || id != 1 {
+			t.Errorf("Expected id to be 1. Got %v", result["data"].(map[string]interface{})["id"])
+		}
+
+		// Check for title
+		if title, ok := result["data"].(map[string]interface{})["title"].(string); !ok || title != "Tasks" {
+			t.Errorf("Expected title to be 'Tasks'. Got %v", result["data"].(map[string]interface{})["title"])
+		}
+
+		// Check for CreatedAt presence
+		if _, ok := result["data"].(map[string]interface{})["created_at"]; !ok {
+			t.Errorf("Expected 'created_at' field to be present")
+		}
+	})
+
+	t.Run("expects to get a list", func(t *testing.T) {
+		clearTable()
+
+		payload := []byte(`{"title": "Tasks"}`)
+		req, _ := http.NewRequest("POST", "/api/v1/lists", bytes.NewReader(payload))
+		response := executeRequest(req)
+
+		checkResponseCode(t, http.StatusCreated, response.Code)
+
+		req, _ = http.NewRequest("GET", "/api/v1/lists", nil)
+		response = executeRequest(req)
+
+		checkResponseCode(t, http.StatusOK, response.Code)
+
+		var actual Response
+		json.Unmarshal(response.Body.Bytes(), &actual)
+
+		for _, item := range actual.Data {
+			list, ok := item.(map[string]interface{})
+			if !ok {
+				t.Errorf("Expected list item to be a map. Got '%v'", item)
+			}
+
+			if id, ok := list["id"].(float64); !ok || id != 1 {
+				t.Errorf("Expected id to be 1. Got '%v'", list["id"])
+			}
+
+			if title, ok := list["title"].(string); !ok || title != "Tasks" {
+				t.Errorf("Expected title to be 'Tasks'. Got '%v'", list["title"])
+			}
+
+			if _, ok := list["created_at"]; !ok {
+				t.Errorf("Expected 'created_at' field to be present")
+			}
+		}
+	})
 }
