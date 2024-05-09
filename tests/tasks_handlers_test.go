e0b6e0b09796c98211e4c60c01322d86422a463a
Pablo Bello <pablobello@hey.com>
2024-05-09 17:34:18 +0100

    Add task done handler

--- tests/tasks_handlers_test.go
+++ tests/tasks_handlers_test.go
@@ -142,6 +142,45 @@ func TestTasks(t *testing.T) {
 			"Expected list_id to be 1")
 	})
 
+	t.Run("expects to mark task as done", func(t *testing.T) {
+		clearTableTasksAndLists()
+
+		payload := []byte(`{"title": "Tasks"}`)
+		req, _ := http.NewRequest("POST", "/api/v1/lists", bytes.NewReader(payload))
+		response := executeRequest(req)
+
+		checkResponseCode(t, http.StatusCreated, response.Code)
+
+		payload = []byte(`{"title": "Task 1"}`)
+		req, _ = http.NewRequest("POST", "/api/v1/lists/1/tasks", bytes.NewReader(payload))
+		response = executeRequest(req)
+
+		checkResponseCode(t, http.StatusCreated, response.Code)
+
+		req, _ = http.NewRequest("PATCH", "/api/v1/lists/1/tasks/1/done", nil)
+		response = executeRequest(req)
+
+		checkResponseCode(t, http.StatusOK, response.Code)
+
+		var result map[string]interface{}
+		err := json.Unmarshal(response.Body.Bytes(), &result)
+		if err != nil {
+			t.Errorf("Error unmarshalling response: %v", err)
+			return
+		}
+
+		assertEqual(t, "Task marked as done successfully", result["message"],
+			"Expected message to be 'Task marked as done successfully'")
+		assertEqual(t, float64(1), result["data"].(map[string]interface{})["id"],
+			"Expected id to be 1")
+		assertEqual(t, "Task 1", result["data"].(map[string]interface{})["title"],
+			"Expected title to be 'Task 1'")
+		assertEqual(t, float64(1), result["data"].(map[string]interface{})["list_id"],
+			"Expected list_id to be 1")
+		assertEqual(t, true, result["data"].(map[string]interface{})["done"],
+			"Expected done to be true")
+	})
+
 	t.Run("expects to delete a task", func(t *testing.T) {
 		clearTableTasksAndLists()
 
